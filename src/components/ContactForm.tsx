9a93f079b264ba5643f039ecfd84e37691f4b266
Ryushin Wells <ryushin.wells@gmail.com>
2025-07-04 23:52:40 -0400

    optimized images

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -15,7 +15,7 @@ const contactFormSchema = z.object({
   firstName: z
     .string()
     .min(2, "First name must be at least 2 characters")
-    .max(50, "First name must be less than 50 characters"),
+    .max(50, "First name must be less than 51 characters"),
   lastName: z
     .string()
     .min(2, "Last name must be at least 2 characters")
